b831e4248583b1724fc239e9abe7841fe4bcf656
yuvalgrin <juvs2k@gmail.com>
2020-06-05 14:06:21 +0300

    * .gitignore: Structure
    * FlightMobileApp.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * FlightMobileApp.csproj:
    * appsettings.Development.json:
    * launchSettings.json:
    * ScreenshotController.cs:

---
+++ FlightMobileApp/Controllers/ScreenshotController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FlightMobileApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightMobileApp.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class ScreenshotController : Controller
+    {
+        private ISimulatorConnector _simulatorConnector;
+
+        public ScreenshotController(ISimulatorConnector simulatorConnector)
+        {
+            this._simulatorConnector = simulatorConnector;
+        }
+
+        [HttpPost]
+        public IActionResult AddServer([FromBody] FlightCommand flightCommand)
+        {
+            if (_simulatorConnector.sendCommand(flightCommand))
+                return Ok();
+            return BadRequest("Could not add server.");
+        }
+
+    }
+}
